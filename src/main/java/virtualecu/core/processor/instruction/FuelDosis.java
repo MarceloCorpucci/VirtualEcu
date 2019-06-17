9f657c09ac9681b70aa0c50c8fbbcd48eebe4835
Marcelo Corpucci <mcorpucci@gmail.com>
2019-06-17 17:40:23 -0300

    Added FuelDosis class which is required to the exercise.

---
+++ src/main/java/virtualecu/core/processor/instruction/FuelDosis.java
@@ -0,0 +1,8 @@
+package virtualecu.core.processor.instruction;
+
+public class FuelDosis {
+	public static final float STAGE_O = 1.5f;
+	public static final float STAGE_1 = 2.0f;
+	public static final float STAGE_2 = 2.5f;
+	public static final float STAGE_3 = 3.0f;
+}
